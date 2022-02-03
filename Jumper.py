032be0fb18279fd0c504e37d13f55cb85b55938d
MeganWestover <97703618+MeganWestover@users.noreply.github.com>
2022-02-02 20:49:29 -0700

    Initial commit

---
+++ Jumper.py
@@ -0,0 +1,37 @@
+class Director: 
+
+    def __init__(self):
+        self.is_playing = True
+
+    def start_game(self):
+        
+
+    
+    def _get_inputs(self):
+        self.guess = input("Guess a letter [a-z]: ")
+    
+    def _do_updates(self):
+
+
+    def _do_inputs(self):
+
+
+
+
+class Word:
+
+    def __init__(self):
+
+
+    def hidden_word(self):
+
+    
+    def letter_check(self, )
+    
+
+class Game_board:
+
+
+
+
+    
