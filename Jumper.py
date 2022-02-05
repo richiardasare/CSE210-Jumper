12638f97992e3c2902e05c11466aef04f566980c
luc12001 <luc12001@byui.edu>
2022-02-05 08:22:21 -0600

    Added Class for the array of words to pick from.

--- Jumper.py
+++ Jumper.py
@@ -32,6 +32,10 @@ class Word:
 class Game_board:
 
 
+class WordArray:
+    def __init__(self):
 
+    def Array():
+        self.array = ["Hello", "Shire", "Bring", "Acorn", "Please", "Close", "Front", "Frame", "Zebra", "Libra", "Speed", "Creed", "Anime", "Silly", "Smart", "Crazy", "Learn", "Stern", "Koala", "Brand", "Stand",  "Happy", "Style", "Miles", "Arrow", "Drama", "Allow", "Apply", "Child", "Crime", "Dress", "Dream", "Drink", "Enemy", "Entry", "Focus", "Fruit", "Glass", "Green", "Group", "Heart", "Guide", "Jones", "Judge", "Knife", "Layer", "March", "Major", "Metal", "Money", "Month", "Motor", "Music", "Panel", "Owner", "North", "Night", "Plane", "Plant", "Point", "Power", "Reply", "River", "Round", "Route", "Rugby", "Scale", "Scene", "Shape", "Smoke", "Sound", "South", "Space", "Sport", "Stock"]
 
     
